787cf7eda970a7858516c490f8b4391c373d401d
Magambo Gatete Elie <eliemagambo@gmail.com>
2014-10-23 13:26:12 +0900

    Added exercices from learn ruby the hard way chapter 1

---
+++ chap1/string.rb
@@ -0,0 +1,16 @@
+ type_of_people=10
+ x="There are #{type_of_people} types of people"
+ binary="binary"
+ do_not="don't"
+ y="Those who know #{binary} and those who #{do_not}"
+
+ puts x
+ puts y
+
+ puts "I said: #{x}"
+ puts "I also said: '#{y}'"
+
+ hilarious=false
+ joke_evaluation="Isnt't that joke so funny?,#{!hilarious}"
+ puts joke_evaluation
+ 
